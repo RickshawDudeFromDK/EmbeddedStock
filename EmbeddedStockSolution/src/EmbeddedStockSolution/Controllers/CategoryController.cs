7586779d3f4e1aec237300b889995c0ec89e1d10
mathiastambjergsteengaard <mathiastambjergiversen@gmail.com>
2016-12-01 08:20:24 +0100

    categories added

--- EmbeddedStockSolution/src/EmbeddedStockSolution/Controllers/CategoryController.cs
+++ EmbeddedStockSolution/src/EmbeddedStockSolution/Controllers/CategoryController.cs
@@ -3,13 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using EmbeddedStockSolution.Models;
 
 namespace EmbeddedStockSolution.Controllers
 {
     public class CategoryController : Controller
     {
+        private List<int> list;
+
         public IActionResult Index()
         {
+            list = new List<int>{1,2,3,4};
+            ViewBag.list = list;
+            return View();
+        }
+
+        public IActionResult New()
+        { 
+            list = new List<int>{1,2,3,4};
+            ViewBag.list = list;
+            return View();
+        }
+        
+        public IActionResult Create()
+        { 
+            list = new List<int>{1,2,3,4};
+            ViewBag.list = list;
+            return RedirectToAction("Category", "Index", new { area = "" });
+        }
+
+        public IActionResult Show()
+        {
+            list = new List<int>{1,2,3,4};
+            ViewBag.list = list;
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+
+            list = new List<int>{1,2,3,4};
+            ViewBag.list = list;
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+
+            list = new List<int>{1,2,3,4};
+            ViewBag.list = list;
             return View();
         }
 
